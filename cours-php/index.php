a771d961f8f1310cfe7dbb8da773024b2b79b2b1
Kabendou41 <balamini@DESKTOP-TDGK0ID.>
2025-09-01 12:28:09 +0200

    main

---
+++ cours-php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cours PHP</title>
+</head>
+<body>
+    <H1>Les bases en PHP</H1>
+</body>
+</html>
